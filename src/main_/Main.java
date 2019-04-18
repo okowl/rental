da346b870a84f91f3357d90cb736c29392db0931
olgak <olli.kiseleva@gmail.com>
2019-04-18 10:25:22 +0100

    Some modules were removed

--- src/main_/Main.java
+++ src/main_/Main.java
@@ -1,15 +1,11 @@
 package main_;
 
-import views.Log_in_screen;
-
-import javax.swing.*;
-import java.sql.SQLException;
-
-public class Main extends JFrame {
 
 
-    public static void main(String... args) throws SQLException {
+public class Main {
 
-        new Log_in_screen();
-    }
+
+public static void Main(){
+    new Main();
+}
 }
