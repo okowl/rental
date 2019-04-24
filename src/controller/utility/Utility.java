c436fa2026d08f899107010d8d8677c62c841c9f
olgak <olli.kiseleva@gmail.com>
2019-04-24 21:12:53 +0100

    more models to the god of models, and more comments

---
+++ src/controller/utility/Utility.java
@@ -0,0 +1,18 @@
+package controller.utility;
+
+public class Utility {
+
+    /**
+     * Just class with some universal methods
+     */
+
+    public void prnt(String text){
+
+        System.out.println("----------------------------------------\n");
+        System.out.println(text);
+        System.out.println("\n----------------------------------------");
+    }
+
+    
+
+}
